12ba771db33b4c1a2eee24b8f04c8c58d57cf200
BenBarahona <benjamin@helloiconic.com>
2024-06-08 11:59:57 -0700

    Reverted impact categories to default

--- src/config.ts
+++ src/config.ts
@@ -63,20 +63,12 @@ export const eas = {
   },
 };
 
-// export const impactCategories = {
-//   ETHEREUM_INFRASTRUCTURE: { label: "Ethereum Infrastructure" },
-//   OPEN_SOURCE: { label: "Web3 Open Source Software" },
-//   COMMUNITY_EDUCATION: { label: "Web3 Community & Education" },
-//   COLLECTIVE_GOVERNANCE: { label: "Collective Governance" },
-//   OP_STACK: { label: "OP Stack" },
-//   DEVELOPER_ECOSYSTEM: { label: "Developer Ecosystem" },
-//   END_USER_EXPERIENCE_AND_ADOPTION: { label: "End user UX" },
-// } as const;
-
 export const impactCategories = {
-  ADOPCION: { label: "Adopción de Ethereum, Blockchain / Web3" },
-  INNOVACION: { label: "Inovación" },
-  SALUD: { label: "Salud" },
-  ARTE_CREATIVIDAD: { label: "Arte y Creatividad" },
-  MEDIO_AMBIENTE: {label: "Medio AMbiente"}
+  ETHEREUM_INFRASTRUCTURE: { label: "Ethereum Infrastructure" },
+  OPEN_SOURCE: { label: "Web3 Open Source Software" },
+  COMMUNITY_EDUCATION: { label: "Web3 Community & Education" },
+  COLLECTIVE_GOVERNANCE: { label: "Collective Governance" },
+  OP_STACK: { label: "OP Stack" },
+  DEVELOPER_ECOSYSTEM: { label: "Developer Ecosystem" },
+  END_USER_EXPERIENCE_AND_ADOPTION: { label: "End user UX" },
 } as const;
